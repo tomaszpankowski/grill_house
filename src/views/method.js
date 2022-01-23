5fcdbd743083318e7f8f2c404c7c9b6c5968980e
tomaszpankowski <pankowski.email@gmail.com>
2022-01-23 14:48:32 +0100

    routing modfied

---
+++ src/views/method.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Method extends Component{
+    render(){
+        return(        
+            <Container fluid className="method-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Method;
